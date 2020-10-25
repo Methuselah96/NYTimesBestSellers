2d42d529493ea53fe03e98983b753f1aae73321a
Nathan Bierema <nbierema@gmail.com>
2020-10-24 23:55:37 -0400

    More versatile

--- NYTimesBestSellers/NYTimesBestSellers/Program.cs
+++ NYTimesBestSellers/NYTimesBestSellers/Program.cs
@@ -1,36 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using MoreLinq.Extensions;
 
 namespace NYTimesBestSellers
 {
     class Result
     {
-        public bool Failed { get; set; }
         public List<string> Titles { get; set; }
-        public DateTime DateTime { get; set; }
-    }
-
-    public static class BatchExtension
-    {
-        public static async Task<List<TResult>> BatchRequests<TResult>(this IEnumerable<Task<TResult>> requests, int batchSize)
-        {
-            var results = new List<TResult>();
-
-            foreach (var requestsBatch in requests.Batch(batchSize))
-            {
-                results.AddRange(await Task.WhenAll(requestsBatch));
-            }
-
-            return results;
-        }
     }
 
     class Program
@@ -47,26 +28,22 @@ namespace NYTimesBestSellers
                 dateTimes.Add(loopDateTime);
 
                 loopDateTime = loopDateTime.AddDays(7);
-                // if (loopDateTime == new DateTime(2011, 4, 3))
-                // {
-                //     loopDateTime = loopDateTime.AddDays(1);
-                // }
             }
 
             var results = new List<Result>();
             foreach (var dateTime in dateTimes)
             {
-                var result = await MakeRequest(dateTime);
-                results.Add(result);
+                var fictionResult = await MakeRequest(dateTime, "combined-print-and-e-book-fiction");
+                var nonFictionResult = await MakeRequest(dateTime, "combined-print-and-e-book-nonfiction");
+                results.Add(fictionResult);
+                results.Add(nonFictionResult);
             }
 
-            var books = new Dictionary<string, int>();
-            var max = 0;
+            var books = new Dictionary<string, (int Points, int Count)>();
             const int maxPoints = 15;
-            foreach (var result in results.Where(result => !result.Failed))
+            foreach (var result in results)
             {
                 var titles = result.Titles;
-                max = titles.Count;
                 if (titles.Count > maxPoints)
                 {
                     throw new Exception($"Max book titles is at least {titles.Count}");
@@ -83,11 +60,11 @@ namespace NYTimesBestSellers
                     var points = maxPoints - i;
                     if (books.ContainsKey(title))
                     {
-                        books[title] = books[title] + points;
+                        books[title] = (books[title].Points + points, books[title].Count + 1);
                     }
                     else
                     {
-                        books[title] = points;
+                        books[title] = (points, 1);
                     }
                 }
             }
@@ -95,48 +72,29 @@ namespace NYTimesBestSellers
             var stringBuilder = new StringBuilder();
             foreach (var (key, value) in books)
             {
-                var key2 = key.Contains(",") ? $"\"{key}\"" : key;
-                stringBuilder.AppendLine($"{key2},{value}");
+                var escapedKey = key.Contains(",") ? $"\"{key}\"" : key;
+                stringBuilder.AppendLine($"{escapedKey},{value}");
             }
 
             var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "nyt-non.csv");
             await File.WriteAllTextAsync(filePath, stringBuilder.ToString());
-
-            Console.WriteLine("Failed:");
-            foreach (var result in results.Where(result => result.Failed))
-            {
-                Console.WriteLine($"{result.DateTime}");
-            }
-            Console.WriteLine($"Max: {max}");
         }
 
-        public static async Task<Result> MakeRequest(DateTime dateTime)
+        public static async Task<Result> MakeRequest(DateTime dateTime, string category)
         {
             var url =
-                $"https://www.nytimes.com/books/best-sellers/{dateTime.Year}/{dateTime.Month:D2}/{dateTime.Day:D2}/combined-print-and-e-book-nonfiction/";
+                $"https://www.nytimes.com/books/best-sellers/{dateTime.Year}/{dateTime.Month:D2}/{dateTime.Day:D2}/{category}/";
             var response = await Client.GetAsync(url);
 
             var j = 1;
             while (!response.IsSuccessStatusCode)
             {
-                if (j > 16)
-                {
-                    ;
-                }
                 Thread.Sleep(j * 1000);
                 response = await Client.GetAsync(url);
                 j *= 2;
             }
             Console.WriteLine(dateTime);
 
-            if (!response.IsSuccessStatusCode)
-            {
-                return new Result
-                {
-                    Failed = true,
-                    DateTime = dateTime,
-                };
-            }
             var responseBody = await response.Content.ReadAsStringAsync();
 
             const string pattern = @"itemProp=""name"">([^<]+)<\/h3>";
